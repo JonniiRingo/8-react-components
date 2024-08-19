5fa63c3b562a7fbb97abd59d0bc7a0a9e198625c
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-08-18 20:29:14 -0700

    Challenge 1: Create List Component

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -6,4 +6,6 @@ function Heading(){
     return <h1>Hello World!</h1>;
   }
 
+
+
   export default Heading;
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client"; 
-import Heading from ".Heading"          // The extension .jsx is optional
+import Heading from "./Heading";       // The extension .jsx is optional
+import List from "./List";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,12 +14,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div>
-      <Heading />     //Custom heading component
-      <ul>
-        <li>One</li>
-        <li>Two</li>
-        <li>Three</li>
-      </ul>
+      <Heading />     
+      <List />       
     </div>
   </React.StrictMode>
 ); 
